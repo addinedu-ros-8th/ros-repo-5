06e91f81b58f0b093074da932edf87deefe824b3
kimsnake <khkh6380@gmail.com>
2025-04-24 15:48:13 +0900

    controll server init

---
+++ admin/src/controll_server_package/controll_server_pkg/controll_server_pkg/drive_router_node.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from ai_server_package_msgs.msg import CmdAndPinkyNum
+
+class DriveRouterNode(Node):
+    def __init__(self):
+        super().__init__('drive_router_node')
+        self.subscription = self.create_subscription(
+            CmdAndPinkyNum,
+            'drive',  # 예시 topic
+            self.listener_callback,
+            10
+        )
+        self.get_logger().info("🚦 Drive Router Node Started")
+
+    def listener_callback(self, msg):
+        self.get_logger().info(
+            f"📬 Received: Robot ID={msg.pinky_num}, X={msg.x}, Y={msg.y}"
+        )
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DriveRouterNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
--- admin/src/controll_server_package/controll_server_pkg/setup.py
+++ admin/src/controll_server_package/controll_server_pkg/setup.py
@@ -1,25 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'controll_server_pkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jack',
     maintainer_email='khkh6380@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Drive router node using custom MoveCommand message',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'drive_router_node = controll_server_pkg.drive_router_node:main'
         ],
     },
 )
